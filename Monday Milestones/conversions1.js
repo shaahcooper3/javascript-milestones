70cdeb16baeecfec03427816e93ad98e8661132d
Shaah Cooper <shaahcooper3@flakko.local>
2020-02-17 19:35:24 -0800

    second attempt at conversions

--- Monday Milestones/conversions1.js
+++ Monday Milestones/conversions1.js
@@ -2,6 +2,8 @@
 let num = 15
 let number = num.toString(); // Expected output
 console.log(number);
+
+
 // string to number
 console.log(parseInt(number));
 
@@ -11,10 +13,15 @@ function strToChar(string) {
   return string.split('');
 }
 
+
+// join string
+let coolArray = ['Atlanta', 'Houston', 'LosAngeles', 'Dallas', 'Charlotte']
+console.log(coolArray.join(''));
+console.log(coolArray.join());
+console.log(coolArray.join(', '));
+
+
 if (require.main === module) {
   console.log('Running sanity checks for strToChar:');
   console.log(strToChar('Hello World!'))
 }
-module.exports = strToChar
-
-// a
